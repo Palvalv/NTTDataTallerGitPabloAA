c278f3dad2437c40c385ded0374bc065e0d09a9d
Usuario <usuario@MacBook-Air-de-Usuario.local>
2022-04-29 11:51:24 +0200

    primer commit

---
+++ src/com/nttdatagitpabloaa/App.java
@@ -0,0 +1,16 @@
+package com.nttdatagitpabloaa;
+
+public class App {
+
+	/**
+	 * @author PAA
+	 */
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola mundo");
+		System.out.println("Hello PABLO");
+
+	}
+
+}
